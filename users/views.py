d8c85e69aa52b321004b297ad8d1b55be47f4e1c
Asus <Asus@Asus>
2021-06-24 15:25:17 +0300

    Добавлено представление регистрации нового пользователя.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def register(request):
+    """регистрирует нового пользователя."""
+    if request.method != 'POST':
+        """Выводим пустую форму регистрации."""
+        form = UserCreationForm()
+    else:
+        """Обработка заполненной формы."""
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            """Выполнение входа и перенаправление на домашнюю страницу."""
+            login(request, new_user)
+            return redirect('blogs:index')
+
+    """Выыести пустую или недействительную форму."""
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
